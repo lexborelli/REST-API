f034c1e0e83312c67c246a8bc9d30a840ab9c7b3
lexborelli <alexasborelli@gmail.com>
2024-05-30 11:46:21 -0400

    added one to many association between user and course. added one to one with course and user

--- models/course.js
+++ models/course.js
@@ -3,24 +3,37 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Course extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
+  class Course extends Model {}
   Course.init({
-    title: DataTypes.STRING,
-    description: DataTypes.TEXT,
-    estimatedTime: DataTypes.STRING,
-    materialsNeeded: DataTypes.STRING
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }, 
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    estimatedTime: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    materialsNeeded: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   }, {
     sequelize,
     modelName: 'Course',
   });
+
+  Course.associate = (models) => {
+    Course.belongsTo(models.User, {
+      foreignKey: {
+        fieldName: 'userId',
+        allowNull: false,
+      },
+    });
+  };
+
   return Course;
 };
--- models/user.js
+++ models/user.js
@@ -3,24 +3,41 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
+  class User extends Model {}
+
   User.init({
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    emailAddress: DataTypes.STRING,
-    password: DataTypes.STRING
-  }, {
+
+    firstName: { 
+      type: DataTypes.STRING,
+      allowNull: false, 
+    }, 
+    lastName: { 
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    emailAddress: { 
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+      password: { 
+        type: DataTypes.STRING,
+        allowNull: false,
+  },
+ }, { 
     sequelize,
     modelName: 'User',
-  });
+ });
+
+ // In the Users model, added a one-to-many association between the User and Course models using the hasMany() method.
+
+  User.associate = (models) => {
+    User.hasMany(models.Movie, {
+      foreignKey: {
+       fieldName: 'userId',
+       allowNull: false,
+      },
+    });
+  };
+
   return User;
 };
