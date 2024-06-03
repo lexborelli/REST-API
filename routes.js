55dcd607655da0120c118f941c391d954a9f733f
lexborelli <alexasborelli@gmail.com>
2024-06-02 21:42:55 -0400

    created the delete route

--- routes.js
+++ routes.js
@@ -82,3 +82,63 @@ router.get('/courses', asyncHandler(async (req, res) => {
     res.status(200).json(courses);
 }));
 
+//api/courses/:-GET: Return the corresponding course including the User object associated with that course and a 200 HTTP status code.
+router.get('/courses/:id', asyncHandler(async (req, res) => {
+    const course = await Course.findByPk(req.params.id, { 
+        include: [
+            {
+            Model: 'User',
+            as: 'user',
+            }
+        ]
+    });
+    res.status(200).json(course);
+}));
+
+//api/courses - POST: Create a new course, set the Location header to the URI for the newly created course, and return a 201 HTTP status code and no content.
+router.post('/courses', asyncHandler(async (req, res) => {
+    try {
+        const course =  await Course.create(req.body);
+        //set the Location header to "/", and return a 201 HTTP status code and no content.
+        res.status(201).location(`/courses/${course.id}`).end();
+        } catch(err) {
+            res.status(500).json({message: err.message});
+        }
+    }));
+
+//api/courses/:id - PUT: Update the corresponding course and return a 204 HTTP status code and no content.
+
+router.put('/courses/:id', asyncHandler(async (req, res) => {
+    
+    try {
+      const course = await course.findByPk(req.params.id);
+     
+      if (course.id === req.currentUser.id) {
+        await course.update(req.body);
+        res.status(204).end();
+      } else {
+        res.status(404).json({message: 'Couse was not found'});
+      }
+    
+    } catch(err) {
+        res.status(500).json({message: err.message});
+    }
+}));
+
+
+//api/courses/:id - DELETE: Delete the corresponding course and return a 204 HTTP status code and no content.
+router.delete('/courses/:id', asyncHandler(async(req, res) => {
+    try {
+        const course = await Course.findByPk(req.params.id); 
+        if (course) {
+            course.destroy(); 
+            res.status(204).end();
+        } else {
+            res.status(404).json({message: "Quote not found"});
+        }
+    } catch (err) {
+        res.status(500).json({message: err.message});
+
+    }
+}));
+
